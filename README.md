d15525cf259b8de10e5d593245a8e2c1f34221c5
Chris Howe-Jones <chris.howejones@gmail.com>
2018-01-10 11:45:28 +0000

    Initial checkin

---
+++ README.md
@@ -0,0 +1,14 @@
+# clojure-game-geek
+
+A lacinia tutorial.
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2018 Chris Howe-Jones
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ dev-resources/user.clj
@@ -0,0 +1,34 @@
+(ns user
+  (:require [clojure-game-geek.schema :as s]
+            [clojure.walk :as walk]
+            [com.walmartlabs.lacinia :as lacinia])
+  (:import clojure.lang.IPersistentMap))
+
+(def schema (s/load-schema))
+
+(defn simplify
+  "Converts all ordered maps nested within the map into standard hash
+  maps, and sequences into vectors, whcih makes for easier constants
+  in the tests, and eliminates ordering problems"
+  [m]
+  (walk/postwalk
+   (fn [node]
+     (cond
+       (instance? IPersistentMap node) (into {} node)
+       (seq? node) (vec node)
+       :else node))
+   m))
+
+(defn q
+  [query-string]
+  (simplify (lacinia/execute schema query-string nil nil)))
+
+(comment
+
+  (q "{ game_by_id(id: \"1234\") { id name summary}}")
+
+  (q "{ game_by_id(id: \"1237\") { name designers { name }}}")
+
+
+
+  )
