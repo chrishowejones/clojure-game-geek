fd0ec29d0038626f78557b819a3dd56e867e7d3d
Chris Howe-Jones <chris.howejones@gmail.com>
2018-01-13 09:52:06 +0000

    Added Stuart Sierra's components.

--- src/clojure_game_geek/schema.clj
+++ src/clojure_game_geek/schema.clj
@@ -1,15 +1,15 @@
 (ns clojure-game-geek.schema
   "Contains custom resolvers and a function to provide the full schema."
-  (:require
-   [clojure.java.io :as io]
-   [com.walmartlabs.lacinia.util :as util]
-   [com.walmartlabs.lacinia.schema :as schema]
-   [clojure.edn :as edn]))
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [com.stuartsierra.component :as component]
+            [com.walmartlabs.lacinia.schema :as schema]
+            [com.walmartlabs.lacinia.util :as util]))
 
-(defn resolve-game-by-id
-  [games-map context args value]
+(defn resolve-element-by-id
+  [element-map context args value]
   (let [{:keys [id]} args]
-    (get games-map id)))
+    (get element-map id)))
 
 (defn resolve-board-game-designers
   [designers-map context args board-game]
@@ -29,20 +29,42 @@
   (reduce #(assoc %1 (:id %2) %2) {} (get data k)))
 
 (defn resolver-map
-  []
+  [component]
   (let [cgg-data (-> (io/resource "cgg-data.edn")
                      slurp
                      edn/read-string)
         games-map (entity-map cgg-data :games)
+        members-map (entity-map cgg-data :members)
         designers-map (entity-map cgg-data :designers)]
-    {:query/game-by-id (partial resolve-game-by-id games-map)
+    {:query/game-by-id (partial resolve-element-by-id games-map)
      :BoardGame/designers (partial resolve-board-game-designers designers-map)
      :Designer/games (partial resolve-designer-games games-map)}))
 
 (defn load-schema
-  []
+  [component]
   (-> (io/resource "cgg-schema.edn")
       slurp
       edn/read-string
-      (util/attach-resolvers (resolver-map))
+      (util/attach-resolvers (resolver-map component))
       schema/compile))
+
+(defrecord SchemaProvider [schema]
+  component/Lifecycle
+  (start [this]
+    (assoc this :schema (load-schema this)))
+  (stop [this]
+    (assoc this :schema nil)))
+
+(defn new-schema-provider
+  []
+  {:schema-provider (map->SchemaProvider {})})
+
+(comment
+
+  (resolver-map nil)
+  (-> {}
+      load-schema
+      :QueryRoot
+      :fields)
+
+  )
